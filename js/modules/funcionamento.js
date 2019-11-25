19eaba6aa3d2f7cb1369105530aaebe958afecf4
Dionis Marques <dionismarques@hotmail.com>
2019-11-25 10:15:03 -0300

    funcionamento refatorado

--- js/modules/funcionamento.js
+++ js/modules/funcionamento.js
@@ -1,30 +1,41 @@
-export default function initFuncionamento() {
-  const funcionamento = document.querySelector('[data-semana');
-  const diasSemana = funcionamento.dataset.semana.split(',').map(Number);
-  const horarioSemana = funcionamento.dataset.horario.split(',').map(Number);
+export default class Funcionamento {
+  constructor(funcionamento, activeClass) {
+    this.funcionamento = document.querySelector(funcionamento);
+    this.activeClass = activeClass;
 
-  const dataAgora = new Date();
-  const diaAgora = dataAgora.getDay();
-  const horarioAgora = dataAgora.getHours();
-
-  const semanaAberta = diasSemana.indexOf(diaAgora) !== -1;
-  const horarioAberto = (horarioAgora >= horarioSemana[0] && horarioAgora < horarioSemana[1]);
-
-  if (semanaAberta && horarioAberto) {
-    funcionamento.classList.add('aberto');
   }
-  // console.log(funcionamento)
+
+  dadosFuncionamento() {
+    this.diasSemana = this.funcionamento.dataset.semana.split(',').map(Number);
+    this.horarioSemana = this.funcionamento.dataset.horario.split(',').map(Number);
+  }
+
+  dadosAgora() {
+    this.dataAgora = new Date();
+    this.diaAgora = this.dataAgora.getDay();
+    this.horarioAgora = this.dataAgora.getUTCHours() - 3;
+  }
+
+  estaAberto() {
+    const semanaAberta = this.diasSemana.indexOf(this.diaAgora) !== -1;
+    const horarioAberto = (this.horarioAgora >= this.horarioSemana[0] && this.horarioAgora < this.horarioSemana[1]);
+
+    return semanaAberta && horarioAberto;
+  }
+
+  ativaAberto() {
+    if (this.estaAberto()) {
+      this.funcionamento.classList.add(this.activeClass);
+    }
+  }
+
+  init() {
+    if (this.funcionamento) {
+      this.dadosFuncionamento();
+      this.dadosAgora();
+      this.ativaAberto();
+    }
+    return this;
+  }
 }
-// const agora = new Date();
-// const futuro = new Date('Dec 31 2019 23:59')
-// // 0 = domingo, 1 = segunda
-// console.log(agora.getDay())
-// console.log(futuro)
-// function transformarDias(tempo) {
-//   return tempo / (24 * 60 * 60 * 1000)
-//   // horas
-//   // return tempo / (60 * 60 * 1000)
-// }
-// const diasAgora = transformarDias(agora.getTime())
-// const diasFuturo = transformarDias(futuro.getTime())
-// console.log(diasFuturo - diasAgora)
+
